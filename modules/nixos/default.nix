ec45cc293a45c938c839796de86d8517a2064c46
Nicolas Goudry <goudry.nicolas@gmail.com>
2024-05-19 13:21:02 +0200

    refactor(modules): move options to single file imported by both modules

--- modules/nixos/default.nix
+++ modules/nixos/default.nix
@@ -36,67 +36,11 @@ let
     ${pkgs.coreutils}/bin/echo "Could not detect environment, use feh"
     ${pkgs.feh}/bin/feh ${fehFlags} $file
   '';
+  opts = import ../options.nix { inherit lib; };
 in
 {
   meta.maintainers = [ lib.maintainers.nicolas-goudry ];
-
-  options = {
-    services.earth-view = {
-      enable = lib.mkEnableOption "" // {
-        description = ''
-          Whether to enable Earth View service.
-
-          Note, if you have set up a custom desktop manager session,
-          then the session configuration must have the `bgSupport`
-          option set to `true` or the background image set by this
-          module may be overwritten.
-        '';
-      };
-
-      interval = lib.mkOption {
-        type = with lib.types; nullOr str;
-        default = null;
-        example = "1h";
-        description = ''
-          The duration between changing background image. Set to
-          `null` to only set background when logging in. Should be
-          formatted as a duration understood by systemd.
-        '';
-      };
-
-      imageDirectory = lib.mkOption {
-        type = lib.types.str;
-        default = ".earth-view";
-        example = "backgrounds";
-        description = ''
-          The directory to which background images should be
-          downloaded, relative to HOME.
-        '';
-      };
-
-      display = lib.mkOption {
-        type = lib.types.enum [ "center" "fill" "max" "scale" "tile" ];
-        default = "fill";
-        description = ''
-          Display background images according to this option.
-
-          Note that this option has no effect on GNOME shell desktops.
-        '';
-      };
-
-      enableXinerama = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
-          Will place a separate image per screen when enabled,
-          otherwise a single image will be stretched across all
-          screens.
-
-          Note that this option has no effect on GNOME shell desktops.
-        '';
-      };
-    };
-  };
+  options.services.earth-view = opts;
 
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
