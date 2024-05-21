00580ac10e054c0b5d2b921e601e80cd3cd9c95a
Nicolas Goudry <nicolas-goudry@users.noreply.github.com>
2024-05-21 16:25:49 +0200

    refactor(modules): deduplicate code (#11)

---
+++ modules/common/script.nix
@@ -0,0 +1,41 @@
+{ config, lib, pkgs, ... }:
+
+let
+  inherit ((pkgs.callPackage ../../. { inherit pkgs; })) earth-view;
+
+  cfg = config.services.earth-view;
+
+  fehFlags = lib.concatStringsSep " "
+    ([ "--bg-${cfg.display}" "--no-fehbg" ]
+      ++ lib.optional (!cfg.enableXinerama) "--no-xinerama");
+in
+source:
+pkgs.writeScriptBin "start" ''
+  #!${pkgs.bash}/bin/bash
+
+  outdir="$HOME/${cfg.imageDirectory}"
+
+  ${pkgs.coreutils}/bin/mkdir -p $outdir
+  file=$(${earth-view}/bin/earth-view fetch random -i ${source} -o $outdir)
+
+  if test $? -ne 0; then
+    ${pkgs.coreutils}/bin/echo "Error while fetching image"
+    exit 1
+  fi
+
+  if test "$XDG_CURRENT_DESKTOP" = "GNOME"; then
+    ${pkgs.coreutils}/bin/echo "GNOME detected, use gsettings"
+    ${pkgs.glib}/bin/gsettings set org.gnome.desktop.background picture-uri file://$file
+    ${pkgs.glib}/bin/gsettings set org.gnome.desktop.background picture-uri-dark file://$file
+    exit 0
+  fi
+
+  if test "$XDG_CURRENT_DESKTOP" = "KDE"; then
+    ${pkgs.coreutils}/bin/echo "KDE detected, use plasma-apply-wallpaperimage"
+    ${pkgs.libsForQt5.plasma-workspace}/bin/plasma-apply-wallpaperimage $file
+    exit 0
+  fi
+
+  ${pkgs.coreutils}/bin/echo "Could not detect environment, use feh"
+  ${pkgs.feh}/bin/feh ${fehFlags} $file
+''
--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -1,102 +1,13 @@
-{ config, lib, pkgs, ... }:
+{ config, lib, pkgs, ... }@args:
 
 let
-  inherit ((pkgs.callPackage ../.. { inherit pkgs; })) earth-view;
-
   cfg = config.services.earth-view;
-
-  fehFlags = lib.concatStringsSep " "
-    ([ "--bg-${cfg.display}" "--no-fehbg" ]
-      ++ lib.optional (!cfg.enableXinerama) "--no-xinerama");
-
-  startScript = pkgs.writeScriptBin "start-earth-view" ''
-    #!${pkgs.bash}/bin/bash
-
-    file=$(${earth-view}/bin/earth-view fetch random -i $HOME/$1/.source -o $HOME/$1)
-
-    if test $? -ne 0; then
-      ${pkgs.coreutils}/bin/echo "Error while fetching image"
-      exit 1
-    fi
-
-    if test "$XDG_CURRENT_DESKTOP" = "GNOME"; then
-      ${pkgs.coreutils}/bin/echo "GNOME detected, use gsettings"
-      ${pkgs.glib}/bin/gsettings set org.gnome.desktop.background picture-uri file://$file
-      ${pkgs.glib}/bin/gsettings set org.gnome.desktop.background picture-uri-dark file://$file
-      exit 0
-    fi
-
-    if test "$XDG_CURRENT_DESKTOP" = "KDE"; then
-      ${pkgs.coreutils}/bin/echo "KDE detected, use plasma-apply-wallpaperimage"
-      ${pkgs.libsForQt5.plasma-workspace}/bin/plasma-apply-wallpaperimage $file
-      exit 0
-    fi
-
-    ${pkgs.coreutils}/bin/echo "Could not detect environment, use feh"
-    ${pkgs.feh}/bin/feh ${fehFlags} $file
-  '';
+  common = import ../common args;
+  startScript = common.mkStartScript "$HOME/${cfg.imageDirectory}";
 in
 {
   meta.maintainers = [ lib.maintainers.nicolas-goudry ];
-
-  options = {
-    services.earth-view = {
-      enable = lib.mkEnableOption "" // {
-        description = ''
-          Whether to enable Earth View service.
-
-          Note, if you are using NixOS and have set up a custom
-          desktop manager session for Home Manager, then the session
-          configuration must have the `bgSupport` option set to `true`
-          or the background image set by this module may be
-          overwritten.
-        '';
-      };
-
-      interval = lib.mkOption {
-        type = with lib.types; nullOr str;
-        default = null;
-        example = "1h";
-        description = ''
-          The duration between changing background image. Set to
-          `null` to only set background when logging in. Should be
-          formatted as a duration understood by systemd.
-        '';
-      };
-
-      imageDirectory = lib.mkOption {
-        type = lib.types.str;
-        default = ".earth-view";
-        example = "backgrounds";
-        description = ''
-          The directory to which background images should be
-          downloaded, relative to HOME.
-        '';
-      };
-
-      display = lib.mkOption {
-        type = lib.types.enum [ "center" "fill" "max" "scale" "tile" ];
-        default = "fill";
-        description = ''
-          Display background images according to this option.
-
-          Note that this option has no effect on GNOME shell desktops.
-        '';
-      };
-
-      enableXinerama = lib.mkOption {
-        type = lib.types.bool;
-        default = true;
-        description = ''
-          Will place a separate image per screen when enabled,
-          otherwise a single image will be stretched across all
-          screens.
-
-          Note that this option has no effect on GNOME shell desktops.
-        '';
-      };
-    };
-  };
+  options.services.earth-view = common.options;
 
   config = lib.mkIf cfg.enable (lib.mkMerge [
     {
@@ -119,7 +30,7 @@ in
         Service = {
           Type = "oneshot";
           IOSchedulingClass = "idle";
-          ExecStart = "${startScript}/bin/start-earth-view ${cfg.imageDirectory}";
+          ExecStart = "${startScript}/bin/start";
         };
 
         Install = { WantedBy = [ "graphical-session.target" ]; };
